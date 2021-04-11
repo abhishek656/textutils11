24e62be3a65be3cb8273a5c163b02b340cdf4d07
Abhishek kumar Yadav <57166644+abhishek656@users.noreply.github.com>
2021-04-11 14:36:13 +0530

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e_t)mplh&350&v&21hc52s*!3035lz$pwb^tg1a-7p+*_k=glo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://textutils88.herokuapp.com/"]
 
 
 # Application definition
