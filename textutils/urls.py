749af3b6948fb1ff0abe2c4636e983fce391235a
Abhishek kumar Yadav <57166644+abhishek656@users.noreply.github.com>
2021-04-11 15:28:03 +0530

    Update urls.py

--- textutils/urls.py
+++ textutils/urls.py
@@ -17,15 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-from django.views.static import server
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
     path('about', views.about, name='about'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
 
 ]
